afdce5b4c3b625d0ee875b6e3fa090412fb6847c
asebai <asebai@ingeniance.fr>
2017-05-14 18:32:31 +0200

    Update floatIPconfig.sh

--- floatIPconfig.sh
+++ floatIPconfig.sh
@@ -1,6 +1,3 @@
 echo seeking for a floating IP
 "myArray=($(openstack floating ip list | grep None | sort -R | head -1))"
-echo here is an available IP : "${myArray[3]}"
-echo openstack server add floating ip TSTDEVOPSD01 "${myArray[3]}"
-echo add dominos security group
-echo openstack server add security group TSTDEVOPSD01 DOMINOS
+
