bdd4497398ae75d57d00685c73b8b19acdac310d
asebai <asebai@ingeniance.fr>
2017-05-15 03:21:23 +0200

    Update initUser.cmd

--- initUser.cmd
+++ initUser.cmd
@@ -1,3 +1,14 @@
 #ps1_x86
--ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile 
-  -WindowStyle Hidden -Command "New-ADUser -Name “Amine SEBAI” -GivenName Amine -Surname SEBAI -SamAccountName asebai -UserPrincipalName asebai@ingeniance.fr -AccountPassword (Read-Host -AsSecureString “Mot2P@ss”) -PassThru | Enable-ADAccount"
+
+#-ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile 
+#  -WindowStyle Hidden -Command "New-ADUser -Name “Amine SEBAI” -GivenName Amine -Surname SEBAI -SamAccountName asebai -UserPrincipalName asebai@ingeniance.fr -AccountPassword (Read-Host -AsSecureString “Mot2P@ss”) -PassThru | Enable-ADAccount"
+
+Write-Host "create user"
+NET USER asebai "Mot2P@ss" /ADD
+
+
+Write-Host "set user as administrator"
+NET LOCALGROUP "Administrators" "asebai" /add
+
+
+Write-Host "let us test it now"
