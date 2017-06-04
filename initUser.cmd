a119c53187b4744fb476dbb5c0ef5d48ede8da34
asebai <asebai@ingeniance.fr>
2017-06-04 16:17:58 +0200

    Update initUser.cmd

--- initUser.cmd
+++ initUser.cmd
@@ -1,4 +1,4 @@
-#ps1_x86
+#ps1_sysnative
 
 #-ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile 
 #  -WindowStyle Hidden -Command "New-ADUser -Name “Amine SEBAI” -GivenName Amine -Surname SEBAI -SamAccountName asebai -UserPrincipalName asebai@ingeniance.fr -AccountPassword (Read-Host -AsSecureString “Mot2P@ss”) -PassThru | Enable-ADAccount"
